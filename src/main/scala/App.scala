20752dd21600bb104fc0eab04fd9bcffe7801f71
aerialist7 <32072831+aerialist7@users.noreply.github.com>
2019-04-27 16:24:01 +0400

    ScalaJS: Simple hello-world App

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,9 +1,10 @@
 import org.scalajs.dom
-import dom.document
+import org.scalajs.dom.document
 
 object App {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+
     val div = document.createElement("div")
     div.innerHTML = "Hello World!"
     document.body.appendChild(div)
